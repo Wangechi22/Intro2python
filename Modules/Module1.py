fcc46757a6e3f9647a4bbc884c26054d38beb0d6
unknown <wangechi2000.wn@gmail.com>
2023-07-04 09:01:24 +0300

    modules in python

---
+++ Modules/Module1.py
@@ -0,0 +1,8 @@
+def add2nos(num1, num2):
+    return num1 + num2
+
+def multiply2nos(num1, num2):
+    return num1 * num2
+
+def subtract2nos(num1, num2):
+    return num1 - num2
---
+++ Modules/Module2.py
@@ -0,0 +1,6 @@
+from Module1 import *
+
+print(add2nos(15, 21))
+print(add2nos(20, 22))
+print(multiply2nos(20, 21))
+print(subtract2nos(20,10))
