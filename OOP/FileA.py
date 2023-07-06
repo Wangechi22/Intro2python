126937adf672bf9ea2facf28024a45ac10d271ab
unknown <wangechi2000.wn@gmail.com>
2023-07-06 09:03:05 +0300

    Inheritance

--- OOP/FileA.py
+++ OOP/FileA.py
@@ -1,3 +1,12 @@
 class ParentClass:
     def parentClassmethod(self):
         print("I am the parent and i have been inherited")
+    def addnums(self, x , y):
+        result = x + y
+        print(result)
+    def multiplynums(self, x, y):
+        result = (x * y)
+        print(result)
+    def dividenums(self, x,y ):
+         result = (x/y)
+         print(result)
--- OOP/FileB.py
+++ OOP/FileB.py
@@ -3,3 +3,7 @@ from FileA import ParentClass
 
 object = ParentClass()
 object.parentClassmethod()
+
+object.addnums(10,5)
+object.multiplynums(20,30)
+object.dividenums(20,10)
